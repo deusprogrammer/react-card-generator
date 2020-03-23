e68d6ee28f5e324b51050ae145bb02d9a73bda34
Michael C. Main <deusprogrammer@gmail.com>
2020-03-23 00:14:05 -0500

    FUCK

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
     apiDomain: process.env.REACT_APP_API_DOMAIN || "https://deusprogrammer.com/api/card-svc",
-    imageApiDomain: process.env.IMAGE_API_DOMAIN || "https://deusprogrammer.com/util/img-svc/images"
+    imageApiDomain: process.env.IMAGE_API_DOMAIN || "https://deusprogrammer.com/api/img-svc/images"
 }
