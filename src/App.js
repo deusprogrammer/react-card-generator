928939306acbb77d62008bfe80fe8c6bc31bc7dc
Michael C. Main <deusprogrammer@gmail.com>
2020-04-07 09:57:48 -0500

    Fixed link

--- src/App.js
+++ src/App.js
@@ -16,20 +16,22 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route path={`${process.env.PUBLIC_URL}/cards`} exact component={Cards} />
-          <Route path={`${process.env.PUBLIC_URL}/cards/:id`} exact component={CardDetail} />
-          <Route path={`${process.env.PUBLIC_URL}/new/templates`} exact component={CreateTemplate} />
-          <Route path={`${process.env.PUBLIC_URL}/templates`} exact component={Templates} />
-          <Route path={`${process.env.PUBLIC_URL}/templates/:id`} exact component={Template} />
-          <Route path={`${process.env.PUBLIC_URL}/templates/:id/new/card`} exact component={CreateCard} />
-          <Route path={`${process.env.PUBLIC_URL}/templates/:id/cards`} exact component={CardsByTemplate} />
-          <Redirect to={`${process.env.PUBLIC_URL}/templates`} />
-        </Switch>
-	<div style={{clear: "both", position: "absolute", bottom: "0px", textAlign: "center", width: "100%"}}>
-	  <p>Want to contribute to this project?</p>
-	  <p>Check us out at https://github.com/deusprogrammer/react-card-generator</p>
-	</div>
+        <div style={{paddingBottom: "100px"}}>
+          <Switch>
+            <Route path={`${process.env.PUBLIC_URL}/cards`} exact component={Cards} />
+            <Route path={`${process.env.PUBLIC_URL}/cards/:id`} exact component={CardDetail} />
+            <Route path={`${process.env.PUBLIC_URL}/new/templates`} exact component={CreateTemplate} />
+            <Route path={`${process.env.PUBLIC_URL}/templates`} exact component={Templates} />
+            <Route path={`${process.env.PUBLIC_URL}/templates/:id`} exact component={Template} />
+            <Route path={`${process.env.PUBLIC_URL}/templates/:id/new/card`} exact component={CreateCard} />
+            <Route path={`${process.env.PUBLIC_URL}/templates/:id/cards`} exact component={CardsByTemplate} />
+            <Redirect to={`${process.env.PUBLIC_URL}/templates`} />
+          </Switch>
+        </div>
+        <div style={{clear: "both", position: "fixed", bottom: "0px", textAlign: "center", width: "100%"}}>
+          <p>Want to contribute to this project?</p>
+          <p>Check us out at https://github.com/deusprogrammer/react-card-generator</p>
+        </div>
       </div>
     </Router>
   )
